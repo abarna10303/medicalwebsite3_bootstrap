ef3e544f2e3fffaa461bfc778d73931867df31d6
abarna10303 <abarnas361@gmail.com>
2022-11-24 10:36:26 +0530

    Docter Website

---
+++ docter_website3/main.js
@@ -0,0 +1,36 @@
+$('.g-carousel').owlCarousel({
+    center: true,
+	autoplay:true,
+    items:1,
+    loop:true,
+    margin:10,
+    responsive:{
+		0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+		1000:{
+            items:5
+        },
+    }
+});
+$('.test-carousel').owlCarousel({
+    center: true,
+	autoplay:true,
+    items:1,
+    loop:true,
+    margin:10,
+    responsive:{
+		0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+		1000:{
+            items:3
+        },
+    }
+});
